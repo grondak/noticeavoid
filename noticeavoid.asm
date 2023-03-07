8839bb30258e9e583c92410566bb7cb8f1d3514d
Tony Reynolds <grondak@gmail.com>
2023-03-06 22:26:09 -0500

    added lookieloos and temporary joystick ctrl

--- noticeavoid.asm
+++ noticeavoid.asm
@@ -15,6 +15,8 @@ photoLines          ds 1; a better name
 XPos                ds 0
 YPos                ds 0
 ; for the Lookie Loos Animation
+tempX               ds 1
+tempY               ds 1
 spriteYPosition		ds 1	; 192 is at the top of the screen, the constant VALUE_OF_Y_AT_SCREEN_BOTTOM gives us the bottom.
 currentSpriteLine	ds 1	; (0 &lt;= currentSpriteLine &lt; SPRITE_HEIGHT) for each frame
 hPosition			ds 1
@@ -22,10 +24,8 @@ hPositionFrac		ds 1
 playerBuffer		ds 1
 spriteMoving		ds 1	; Boolean. We use this to see if we stopped moving
 animFrameLineCtr	ds 1
-faceDelay			ds 1
 spriteLineColor		ds 1
 hPositionIndex		ds 1
-faceDuration		ds 1
 
 ;------------------------------------------------
 ; Constants - some made at https://alienbill.com/2600/playerpalnext.html
@@ -36,14 +36,12 @@ THREE_COPIES  = %011        ; draw three copies of the sprite for the photo
 PFFG = $0F                  ; the streets are a concrete jungle
 PFBG = $00                  ; everything else is a shadow
 ; for the Lookie Loos
-FACE_DURATION = 4			; Number of frames each face lasts on screen. Decrease to speed up, increase to slow down.
 FACE_COLOR = $0F            ; bright white
 SLO_MO_FACE_DURATION = 30	; Same as above, applicable when "slo-mo" is activated (i.e. player holds fire button).
 SPRITE_HEIGHT = 8			; Native number of pixels tall the sprite is (before being stretched by a 2LK or whatever).
 NUM_ANIMATION_FACES = 9		; Number of faces of animation. (!)Corresponds with number of color tables(!)
 MIRROR = 1					; If true, sprite mirrors when moved left.
 X_LK = 1					; set to 1 for 1LK, 2 for 2LK, etc.
-SPRITE_WIDTH = 1			; set to 1, 2, or 4, anything else is right out
 BG_COLOR = $00				; background color
 VALUE_OF_Y_AT_SCREEN_BOTTOM = 192-192/X_LK
 VERTICAL_CENTER_OF_SCREEN = 192-(192-VALUE_OF_Y_AT_SCREEN_BOTTOM)/2
@@ -77,33 +75,47 @@ VERTICAL_CENTER_OF_SCREEN = 192-(192-VALUE_OF_Y_AT_SCREEN_BOTTOM)/2
 
     seg	Code    	; start of main segment
     org $F000
+; restart and when reset switch is pulled
 reset:	
     CLEAN_START
-naFrame: ; draw one Notice Avoid Frame
-    VERTICAL_SYNC
-    TIMER_SETUP 37 ; V-Blank
-    jsr photoDraw ; draw the photo first
-; skip 20 more lines for positioning
-    TIMER_SETUP 20 
+    lda #80
+	sta hPositionIndex	; initial x pos for temp lookie loo
+    lda #28
+    sta spriteYPosition	; initial y pos for temp lookie loo
+
+ ; draw one Notice Avoid Frame - 2 line kernel
+naFrame:
+    VERTICAL_SYNC  ;3 lines total 3
+    TIMER_SETUP 37 ; V-Blank 37 lines total 40
+    TIMER_WAIT ;the V-Blank wait
+    ; skip 20 lines for positioning
+    TIMER_SETUP 20 ; 20 lines total 60   
+; draw the photo first ; 75 lines total 135
+    jsr photoDraw 
+; skip 19 more lines for positioning
+    TIMER_SETUP 19 ; 19 lines total 154
     TIMER_WAIT
+; set up the Lookie Loos (1x)
+    jsr tempLoosSetup ; 2 line total 156
     jsr streetsDraw ; draw the streets one line at a time
+                    ; 56 lines total 212
 ; now wait the rest of the screen
-    TIMER_SETUP 32
+    TIMER_SETUP 20 ; 20 lines total 232
     TIMER_WAIT
 ; now wait the overscan
-    TIMER_SETUP 30
+    TIMER_SETUP 30 ; 30 lines total 262
     TIMER_WAIT
     jsr readJoysticks
     jmp naFrame
 
 
-
+; Set up to draw our current photo
 photoDraw:
     lda #75
     sta photoLines	; scanline counter
-    lda #$00
+    lda #PFBG
     sta COLUBK	; background color
-    lda #$0E
+    lda #PFFG
     sta COLUP0	; show how players alternate
     lda #$0E
     sta COLUP1	; by having different colors
@@ -114,6 +126,7 @@ photoDraw:
     SLEEP 20
     sta RESP0	; position 1st player
     sta RESP1	; ...and 2nd player
+    sta HMP0
     lda #$10
     sta HMP1	; 1 pixel to the left
     sta WSYNC
@@ -123,11 +136,10 @@ photoDraw:
     lda #1
     sta VDELP0	; we need the VDEL registers
     sta VDELP1	; so we can do our 4-store trick
-    TIMER_WAIT ; actually wait the v-sync
-    TIMER_SETUP 20 ; skip 20 lines for positioning
-    TIMER_WAIT
+    TIMER_WAIT ; actually wait the remaining lines
     SLEEP 40
-; dump out our player photo
+
+; dump out our player photo line by line
 photoLoop:
     ldy photoLines                  ; 3     (3) 
     lda photo_0,y                   ; 4     (7)
@@ -151,28 +163,32 @@ photoLoop:
     rts
 
 
-streetsDraw:
 ; dump in the mean streets
+streetsDraw:
+    ldy #28     ; 56 total playfield lines because this is a 2-line kernel
+    ldx #7      ; groups of 8 playfield lines
     lda #PFBG
     sta COLUBK
     lda #PFFG
     sta COLUPF
-    ldx #8
-streetsOuter:
-    ldy #7
 streetsInner:
-    sta WSYNC	; wait for next scanline
-    lda pfData0,y
+    sta WSYNC
+    lda pfData0,x
     sta PF0		; set the PF1 playfield pattern register
-    lda pfData1,y
+    lda pfData1,x
     sta PF1		; set the PF1 playfield pattern register
-    lda pfData2,y
+    lda pfData2,x
     sta PF2		; set the PF2 playfield pattern register
+    stx tempX
     jsr tempLoosDraw
+    ldx tempX
     dey
-    bne streetsInner
+    beq streetsDone
     dex
-    bne streetsOuter
+    bne streetsInner
+    ldx #7
+    jmp streetsInner
+streetsDone:
 ; clear out the playfield so it doesn't display when we don't want it.
     sta WSYNC
     lda #0
@@ -183,46 +199,124 @@ streetsInner:
 
 
 
+; prepare to draw a LookieLoo
+tempLoosSetup:
+	lda #$25
+	sta COLUP0
+    sta spriteLineColor
+	lda #0					; set to single
+    sta NUSIZ0
+    sta VDELP0
+    sta VDELP1
+
+	ldx hPositionIndex		;3	|
+	lda hPositionTable,x	;4	|
+	sta hPosition			;3	| hPosition = hPositionTable[hPositionIndex]
+	and #$0F				;2	|
+	tax						;2	| x = (hPosition & $0F) (coarse position)
+    sta WSYNC
+position:
+	dex						;2	| Position Sprite Horizontally (coarse adj.)
+	bne position			;2+	|
+
+	sta RESP0				;3	|
+    ;sta WSYNC
+
+    lda hPosition			;3	|
+	and #$F0				;2	| clear coarse nybble
+	sta HMP0
+    sta WSYNC
+    sta HMOVE
+    lda #0
+    sta spriteMoving        ; override sprite moving
+	lda spriteMoving
+	bne spriteManMoving		;	if (spriteMoving != false) goto SpriteManMoving
+
+	lda #SPRITE_HEIGHT-1	;	// Sprite is idle
+	sta animFrameLineCtr	;	animFrameLineCtr = SPRITE_HEIGHT - 1
+	jmp endAnimationChecks	;	goto EndAnimationChecks
+
+spriteManMoving:			
+	lda animFrameLineCtr	; Sprite is moving
+	cmp #SPRITE_HEIGHT*#NUM_ANIMATION_FACES
+	bcs resetFace			; if (animFrameLineCtr &gt;= height*numFaces) goto ResetFace
+
+	jmp endAnimationChecks	; else goto EndAnimationChecks
+
+resetFace:
+	lda #SPRITE_HEIGHT*#NUM_ANIMATION_FACES-1
+	sta animFrameLineCtr	; animFrameLineCtr = (SPRITE_HEIGHT * NUM_ANIMATION_FACES) - 1
+endAnimationChecks:
+    rts
+
 ; Draw some LookieLoos
 tempLoosDraw:
+    	       
+	; Load Player sprite and color. (10~)
+	lda playerBuffer			;2
+	sta GRP0					;3	GRP0 = playerBuffer
+	lda spriteLineColor			;2
+	sta COLUP0					;3	COLUP0 = spriteLineColor
+	; Clear the playerBuffer. (5~)
+	lda #0						;2
+	sta playerBuffer			;3	playerBuffer = 0    	
+	; See if this is the line where we start drawing the sprite. (Y:10~, N:6~)
+	cpy spriteYPosition			;3
+	bne skipActivatePlayer		;2+	if (y != spriteYPosition) goto SkipActivatePlayer
+
+	lda #SPRITE_HEIGHT-1		;2	else
+	sta currentSpriteLine		;3	currentSpriteLine = SPRITE_HEIGHT-1
+skipActivatePlayer:
+	; See if we are drawing sprite data on this line. (Y:5~, N:6~)
+	lda currentSpriteLine		;3
+	bmi endFaceStuff			;2+	if (currentSpriteLine &lt; 0) goto endFaceStuff
+
+	; Load sprite graphic and color buffers. (20~)
+	ldx animFrameLineCtr		;3
+	lda llGraphicTable,x	    ;4
+	sta playerBuffer			;3	playerBuffer = SpriteGraphicTable[animFrameLineCtr]
+	; Decrement our counters. (10~)
+	dec currentSpriteLine		;5 currentSpriteLine -= 1
+	dec animFrameLineCtr		;5
+	; Manage the frame delay between face animations. 
+endFaceStuff:
     rts
 
 readJoysticks:
 ; Move vertically
-; (up and down are actually reversed since ypos starts at bottom)
-    ldx YPos
-    lda #%00100000	;Up?
+    ldx spriteYPosition
+    lda #%00100000	;Down?
     bit SWCHA
-    bne skipMoveUp
-    cpx #2
-    bcc skipMoveUp
-    dex
-skipMoveUp:
-    lda #%00010000	;Down?
-    bit SWCHA 
     bne skipMoveDown
-    cpx #183
-    bcs skipMoveDown
-    inx
+    cpx #10
+    bcc skipMoveDown
+    dex
 skipMoveDown:
-    stx YPos
+    lda #%00010000	;Up?
+    bit SWCHA 
+    bne skipMoveUp
+    cpx #28
+    bcs skipMoveUp
+    inx
+skipMoveUp:
+    stx spriteYPosition
 ; Move horizontally
-    ldx XPos
+    ldx hPositionIndex
     lda #%01000000	;Left?
     bit SWCHA
     bne skipMoveLeft
-    cpx #16
+    cpx #1
     bcc skipMoveLeft
     dex
 skipMoveLeft:
     lda #%10000000	;Right?
     bit SWCHA 
     bne skipMoveRight
-    cpx #153
+    cpx #152
     bcs skipMoveRight
     inx
 skipMoveRight:
-    stx XPos
+    stx hPositionIndex
     rts
 
 ; https://www.flickr.com/photos/tokyodrifter/4132540774/in/photolist-7ibmp1-7kwDQr-6drtzM-7i7rqD-fVoZL2-283iFVj-7i7rtP-jYc5Bt-B84WBv-7ibmpY-bZTLow-dEY7Uh-qgio3b-ezNHdC-7i7rrr-7i7TLn-2j4x2vd-nfcDxV-4n8mWY-oMAYTH-dEZXBE-uW2BtA-2i4aDit-nYsFmQ-vo5JMz-5T3mhX-NQZLh3-bBAki7-eZwFDo-cm8FiA-2k3G9o1-qCxgBM-edxNEE-69tfxm-4VoNiK-d3aAQh-oZFiyw-nNrGQY-r63Zzb-BCmnBS-f5sW2P-2d3dWDA-agRe5T-a6uZq8-aNupzD-dRJQVC-6b3nTH-D9as7H-5p8iUR-h1bFAk
@@ -708,7 +802,7 @@ photo_5:
     BYTE %00000000
 
 ;---Lookie Loos Graphics Data from PlayerPal 2600--- - https://alienbill.com/2600/playerpalnext.html
-
+llGraphicTable:
 llNeutral:
         .byte #%00000000;--
         .byte #%00111100;--
@@ -822,6 +916,19 @@ pfData2:
         .byte #%11111111
         .byte #%11111111
 
+hPositionTable
+			.byte 				  $34,$24,$14,$04,$F4,$E4,$D4,$C4,$B4,$A4,$94	; 0-10
+			.byte $75,$65,$55,$45,$35,$25,$15,$05,$F5,$E5,$D5,$C5,$B5,$A5,$95	; 11-25
+			.byte $76,$66,$56,$46,$36,$26,$16,$06,$F6,$E6,$D6,$C6,$B6,$A6,$96	; 26-40
+			.byte $77,$67,$57,$47,$37,$27,$17,$07,$F7,$E7,$D7,$C7,$B7,$A7,$97	; 41-55
+			.byte $78,$68,$58,$48,$38,$28,$18,$08,$F8,$E8,$D8,$C8,$B8,$A8,$98	; 56-70
+			.byte $79,$69,$59,$49,$39,$29,$19,$09,$F9,$E9,$D9,$C9,$B9,$A9,$99	; 71-85
+			.byte $7A,$6A,$5A,$4A,$3A,$2A,$1A,$0A,$FA,$EA,$DA,$CA,$BA,$AA,$9A	; 86-100
+			.byte $7B,$6B,$5B,$4B,$3B,$2B,$1B,$0B,$FB,$EB,$DB,$CB,$BB,$AB,$9B	; 101-115
+			.byte $7C,$6C,$5C,$4C,$3C,$2C,$1C,$0C,$FC,$EC,$DC,$CC,$BC,$AC,$9C	; 116-130
+			.byte $7D,$6D,$5D,$4D,$3D,$2D,$1D,$0D,$FD,$ED,$DD,$CD,$BD,$AD,$9D	; 131-145
+			.byte $7E,$6E,$5E,$4E,$3E,$2E,$1E,$0E,$FE,$EE,$DE,$CE,$BE,$AE		; 146-159
+			
     ORG $FFFA
 interruptVectors:
     .word reset          ; NMI
